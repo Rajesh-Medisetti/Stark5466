a8561c7446863ed6b74bfedc8796a70587708e6a
vishal <vk85243@gmail.com>
2021-05-11 19:52:09 +0530

    EQRTR-21 : Validation for OutBoundFeed

---
+++ e2e-suite/src/main/java/entitycreators/OutBoundJobCreator.java
@@ -0,0 +1,30 @@
+package entitycreators;
+
+import com.joveo.eqrtestsdk.core.entities.Client;
+import com.joveo.eqrtestsdk.core.mojo.OutboundFeed;
+import com.joveo.eqrtestsdk.exception.MojoException;
+import com.joveo.eqrtestsdk.models.OutboundJob;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OutBoundJobCreator {
+
+    public static Map<String, OutboundJob> outBoundFeedJob(Client client, String pubId)
+            throws MojoException {
+
+        Map<String, OutboundJob> map = new HashMap<>();
+
+        OutboundFeed outboundFeed = client.getOutboundFeed(pubId);
+
+
+        List<OutboundJob> outboundJobs = outboundFeed.getFeed().getJob();
+
+        for (OutboundJob outboundJob : outboundJobs) {
+
+            map.put(outboundJob.referencenumber, outboundJob); }
+
+
+        return map;
+    }
+}
