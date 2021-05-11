a29d117c7e72ae25a35fa70814a5dc81d86d07f9
Sambhav <sami@Sambhavs-MacBook-Pro.local>
2021-05-11 12:39:29 +0530

    EQRTR-9 : added java code check

--- e2e-suite/src/main/java/enums/RuleOperatorStringNegative.java
+++ e2e-suite/src/main/java/enums/RuleOperatorStringNegative.java
@@ -1,10 +1,9 @@
 package enums;
 
 public enum RuleOperatorStringNegative {
-
   NOT_EQUAL,
-   NOT_IN,
-   NOT_BEGINS_WITH,
-   NOT_CONTAINS,
-   NOT_ENDS_WITH
+  NOT_IN,
+  NOT_BEGINS_WITH,
+  NOT_CONTAINS,
+  NOT_ENDS_WITH
 }
